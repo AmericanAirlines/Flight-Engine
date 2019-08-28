98eb8a2dc6010f920227862ca4964dc41ca85e28
Spencer Kaiser <spencer.kaiser@aa.com>
2019-08-28 16:28:22 -0500

    Initial implementation of flights endpoint
    Co-authored-by: John Kahn <hello@jkahn.me>

---
+++ src/Generator.ts
@@ -0,0 +1,50 @@
+import seedrandom from 'seedrandom';
+import haversine from 'haversine-distance';
+import aircraft from './Data/aircraft';
+
+const createRandomGenerator = (seed: string): (() => number) => {
+  const random = seedrandom(seed);
+  return (min = 0, max = 0): number => {
+    const r = random();
+    return Math.floor(r * (max - min + 1) + min);
+  };
+};
+
+// meters to miles
+const calcDistance = (a: Location, b: Location): number => Math.round(haversine(a, b) / 1609.344);
+
+export default class Generator {
+  random: (min?: number, max?: number) => number;
+
+  constructor(seed: string) {
+    this.random = createRandomGenerator(seed);
+  }
+
+  flight(origin: Airport, destination: Airport): Flight {
+    const flightNumber: string = this.random(1, 9999)
+      .toFixed(0)
+      .padStart(4, '0');
+
+    const distance = calcDistance(origin.location, destination.location);
+
+    const randAircraft = aircraft[this.random(0, aircraft.length - 1)];
+
+    const duration: FlightDuration = {
+      locale: '',
+      hours: (distance / randAircraft.speed) * (this.random(1000, 1100) / 1000),
+      minutes: 0,
+    };
+    duration.minutes = Math.floor(60 * (duration.hours - Math.floor(duration.hours)));
+    duration.hours = Math.floor(duration.hours);
+    duration.locale = `${duration.hours}h ${duration.minutes}m`;
+
+    return {
+      flightNumber,
+      origin,
+      destination,
+      distance,
+      duration,
+      aircraft: randAircraft,
+    };
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { DateTime } from 'luxon';
+import Generator from './Generator';
+import airports from './Data/airports';
 
 const app = express();
 
@@ -12,6 +15,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Establish Routing
 app.get('/', (_: express.Request, res: express.Response) => {
   res.send('👋');
+});
+
+// /flights
+// Retrieve a list of flights for a given day
+// filtered by origin and/or destination
+app.get('/flights', (req, res) => {
+  const seed = DateTime.utc().toISODate();
+  const gen = new Generator(seed);
+  const flights = [];
+
+  for (let i = 0; i < 10; i += 1) {
+    flights.push(gen.flight(airports[0], airports[1]));
+  }
+
+  res.json(flights);
 });
 
 export default app;
