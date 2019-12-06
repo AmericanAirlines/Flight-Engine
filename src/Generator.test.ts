0fb6788d6eb336d2a24701f7ac7fc5c5edf11748
John <hello@jkahn.me>
2019-12-06 10:59:56 -0600

    Fix eslint (#22)
    * Make sure eslint lints all ts files
    
    * Fix linting

--- src/Generator.test.ts
+++ src/Generator.test.ts
@@ -6,67 +6,49 @@ describe('Random seed generator ', () => {
   it('random method returns same values over time using the same seed', () => {
     const seed = '123';
 
-    let generator1 = new Generator(seed);
-    let values1 = new Array(10).fill(null).map(() => {
-      return generator1.random(0, 100);
-    });
+    const generator1 = new Generator(seed);
+    const values1 = new Array(10).fill(null).map(() => generator1.random(0, 100));
 
-    let generator2 = new Generator(seed);
-    let values2 = new Array(10).fill(null).map(() => {
-      return generator2.random(0, 100);
-    });
+    const generator2 = new Generator(seed);
+    const values2 = new Array(10).fill(null).map(() => generator2.random(0, 100));
 
     expect(values1).toEqual(values2);
   });
 
   it('random method returns diff values over time using unique seed', () => {
     const seed1 = '123';
-    let generator1 = new Generator(seed1);
-    let values1 = new Array(10).fill(null).map(() => {
-      return generator1.random(0, 100);
-    });
+    const generator1 = new Generator(seed1);
+    const values1 = new Array(10).fill(null).map(() => generator1.random(0, 100));
 
     const seed2 = '456';
-    let generator2 = new Generator(seed2);
-    let values2 = new Array(10).fill(null).map(() => {
-      return generator2.random(0, 100);
-    });
+    const generator2 = new Generator(seed2);
+    const values2 = new Array(10).fill(null).map(() => generator2.random(0, 100));
 
     expect(values1).not.toEqual(values2);
   });
 
   it('flights generated for a given route will always be the same ', () => {
     const seed = '2020-01-01';
-    let origin = airports[0];
-    let destination = airports[1];
+    const origin = airports[0];
+    const destination = airports[1];
 
-    let generator1 = new Generator(seed);
-    let values1 = new Array(10).fill(null).map(() => {
-      return generator1.flight(origin, destination);
-    });
+    const generator1 = new Generator(seed);
+    const values1 = new Array(10).fill(null).map(() => generator1.flight(origin, destination));
 
-    let generator2 = new Generator(seed);
-    let values2 = new Array(10).fill(null).map(() => {
-        return generator2.flight(origin, destination);
-    });
+    const generator2 = new Generator(seed);
+    const values2 = new Array(10).fill(null).map(() => generator2.flight(origin, destination));
 
     expect(values1).toEqual(values2);
   });
 
-  it('the number of flights generated for a given route on a given day will always be the same ', () => {
+  it('the number of flights generated for on a given day will always be the same ', () => {
     const seed = 'DFW-JFK';
-    let origin = airports[0];
-    let destination = airports[1];
 
-    let generator1 = new Generator(seed);
-    let values1 = new Array(10).fill(null).map(() => {
-      return generator1.numFlightsForRoute();
-    });
+    const generator1 = new Generator(seed);
+    const values1 = new Array(10).fill(null).map(() => generator1.numFlightsForRoute());
 
-    let generator2 = new Generator(seed);
-    let values2 = new Array(10).fill(null).map(() => {
-        return generator2.numFlightsForRoute();
-    });
+    const generator2 = new Generator(seed);
+    const values2 = new Array(10).fill(null).map(() => generator2.numFlightsForRoute());
 
     expect(values1).toEqual(values2);
   });
