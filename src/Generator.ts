3059d978223f302087c962438716fc53502a1d2b
Melinda Malmgren <Melinda.Malmgren@aa.com>
2020-02-15 11:55:01 -0600

    Update src/Generator.ts

--- src/Generator.ts
+++ src/Generator.ts
@@ -70,7 +70,7 @@ export default class Generator {
     // cost is calculated as a percentage of distance, between 10% and 30%
     // this allows for variability in flight prices while maintaining some relativity between values
     // (i.e., longer flights are usually more expensive)
-    const randCost = distance * (this.random(10,30) / 100);
+    const randCost = distance * (this.random(10, 30) / 100);
     return {
       flightNumber,
       origin,
