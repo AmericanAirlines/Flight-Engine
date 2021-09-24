0e7afd14a91d79373cfc0907698ff364ba9e1b57
Demondre Livingston <livingstondemondre7@gmail.com>
2021-09-24 16:39:56 -0400

    adding 21 new airports

--- src/data/airports.ts
+++ src/data/airports.ts
@@ -37,4 +37,193 @@ export const airports: Airport[] = [
       longitude: 87.9073,
     },
   },
+  {
+    code: 'GSO',
+    city: 'Greensboro',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 36.0726,
+      longitude: -79.792,
+    },
+  },
+  {
+    code: 'ATL',
+    city: 'Atlanta',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 33.6404,
+      longitude: -84.4198,
+    },
+  },
+  {
+    code: 'DEN',
+    city: 'Denver',
+    timezone: 'America/Phoenix',
+    location: {
+      latitude: 39.8493,
+      longitude: -104.6738,
+    },
+  },
+  {
+    code: 'CLT',
+    city: 'Charlotte',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 35.2138,
+      longitude: -80.943,
+    },
+  },
+  {
+    code: 'LAS',
+    city: 'Chicago',
+    timezone: 'America/Chicago',
+    location: {
+      latitude: 36.08601,
+      longitude: -115.1539,
+    },
+  },
+  {
+    code: 'PHX',
+    city: 'Phoenix',
+    timezone: 'America/Phoenix',
+    location: {
+      latitude: 36.1043,
+      longitude: -79.935,
+    },
+  },
+  {
+    code: 'MCO',
+    city: 'Orlando',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 28.4179,
+      longitude: -81.3041,
+    },
+  },
+  {
+    code: 'SEA',
+    city: 'Seattle',
+    timezone: 'America/Los_Angeles',
+    location: {
+      latitude: 47.4435,
+      longitude: -122.3016,
+    },
+  },
+  {
+    code: 'MIA',
+    city: 'Miami',
+    timezone: 'America/Chicago',
+    location: {
+      latitude: 25.7969,
+      longitude: -80.2762,
+    },
+  },
+  {
+    code: 'IAH',
+    city: 'Houston',
+    timezone: 'America/Chicago',
+    location: {
+      latitude: 29.9902,
+      longitude: -95.3368,
+    },
+  },
+  {
+    code: 'FLL',
+    city: 'Fort Lauderdale',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 26.0742,
+      longitude: -80.1506,
+    },
+  },
+  {
+    code: 'BWI',
+    city: 'Baltimore',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 39.1774,
+      longitude: -76.6684,
+    },
+  },
+  {
+    code: 'SFO',
+    city: 'San Francisco',
+    timezone: 'America/Los_Angeles',
+    location: {
+      latitude: 37.6213,
+      longitude: -122.379,
+    },
+  },
+  {
+    code: 'EWR',
+    city: 'Newark',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 40.6895,
+      longitude: -74.1745,
+    },
+  },
+  {
+    code: 'MSP',
+    city: 'Minneapolis',
+    timezone: 'America/Chicago',
+    location: {
+      latitude: 44.8848,
+      longitude: -93.2223,
+    },
+  },
+  {
+    code: 'DTW',
+    city: 'Detroit',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 36.1043,
+      longitude: -79.935,
+    },
+  },
+  {
+    code: 'BOS',
+    city: 'Chicago',
+    timezone: 'America/Chicago',
+    location: {
+      latitude: 42.3656,
+      longitude: -71.0096,
+    },
+  },
+  {
+    code: 'TPA',
+    city: 'Tampa',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 36.1043,
+      longitude: -79.935,
+    },
+  },
+  {
+    code: 'SLC',
+    city: 'Salt Lake City',
+    timezone: 'America/Chicago',
+    location: {
+      latitude: 40.7899,
+      longitude: -111.9791,
+    },
+  },
+  {
+    code: 'SAN',
+    city: 'San Diego',
+    timezone: 'America/Los_Angeles',
+    location: {
+      latitude: 32.7338,
+      longitude: -117.1933,
+    },
+  },
+  {
+    code: 'PHL',
+    city: 'Philadelphia',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 39.8729,
+      longitude: -75.2437,
+    },
+  },
 ];
