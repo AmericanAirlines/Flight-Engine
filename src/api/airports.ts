42474068aaf6dc8b917608da799b9274bbf2ebe1
Demondre Livingston <livingstondemondre7@gmail.com>
2021-09-27 10:45:17 -0400

    changes query param to code

--- src/api/airports.ts
+++ src/api/airports.ts
@@ -5,13 +5,13 @@ export const airportRouter = Router();
 
 airportRouter.get('/', (req, res) => {
   const airportCodeFormat = RegExp('[A-Za-z]{3}');
-  const { flightCode } = req.query;
+  const { code } = req.query;
 
-  if (!flightCode || !airportCodeFormat.test(flightCode as string)) {
+  if (!code || !airportCodeFormat.test(code as string)) {
     res.status(400).send('Please enter a valid flight code i.e. DFW, GSO, ATL...');
   }
 
-  const airport = airports.find((port) => port.code.toLowerCase() === flightCode!.toString().toLowerCase());
+  const airport = airports.find((port) => port.code.toLowerCase() === code!.toString().toLowerCase());
 
   if (airport) {
     res.json(airport);
