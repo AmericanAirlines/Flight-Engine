acb4ecd78b7fdcc1157535e127458d747eec29e5
Demondre Livingston <livingstondemondre7@gmail.com>
2021-09-27 11:53:34 -0400

    return to original

--- src/FlightCache.ts
+++ src/FlightCache.ts
@@ -7,6 +7,10 @@ class FlightCache {
     this.flights = {};
   }
 
+  public getFlights(dateString: string): Flight[] {
+    return this.flights[dateString] || null;
+  }
+
   public cacheFlights(dateString: string, flights: Flight[]): void {
     this.flights[dateString] = flights;
   }
