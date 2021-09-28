70c1d572b5d2135e34b4b6f77f903d0fd363be03
Demondre Livingston <livingstondemondre7@gmail.com>
2021-09-28 15:10:02 -0400

    Update tests/api/flights.test.ts
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>

--- tests/api/flights.test.ts
+++ tests/api/flights.test.ts
@@ -13,6 +13,16 @@ jest.mock('../../src/services/generateFlightsBySeed.ts', () => ({
       arrivalTime: '2020-01-01T23:49:40.000-04:00',
       aircraft: { model: '738', passengerCapacity: { total: 160, main: 144, first: 16 }, speed: 400 },
     },
+    {
+      flightNumber: '1234',
+      origin: { code: 'DFW', city: 'Dallas-Fort Worth', timezone: 'America/Chicago', location: { latitude: 32.8998, longitude: 97.0403 } },
+      destination: { code: 'PHL', city: 'Philadelphia', timezone: 'America/New_York', location: { latitude: 39.8729, longitude: -75.2437 } },
+      distance: 7393,
+      duration: { locale: '19h 3m', hours: 19, minutes: 3 },
+      departureTime: '2020-01-01T03:46:40.000-05:00',
+      arrivalTime: '2020-01-01T23:49:40.000-04:00',
+      aircraft: { model: '738', passengerCapacity: { total: 160, main: 144, first: 16 }, speed: 400 },
+    },
   ]),
 }));
 
