549b00b3536e2789124ccb472e2f1b5c0cbd7e14
Demondre Livingston <livingstondemondre7@gmail.com>
2021-09-29 15:32:47 -0400

    Update tests/services/generateFlightsByDate.test.ts
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>

--- tests/services/generateFlightsByDate.test.ts
+++ tests/services/generateFlightsByDate.test.ts
@@ -2,26 +2,24 @@ import 'jest';
 import { DateTime } from 'luxon';
 import { generateFlightsByDate } from '../../src/services/generateFlightsByDate';
 
-describe('FlightCache ', () => {
+describe('generateFlightsByDate ', () => {
   const dateString = '2021-01-01';
   const isoDate = DateTime.fromISO(dateString, { zone: 'utc' });
-
-  beforeEach(() => {
-    jest.resetModules();
-  });
 
   it('should generate flights by seed', () => {
     const flights = generateFlightsByDate(isoDate);
     expect(flights.length).toBeGreaterThan(0);
   });
 
-  it('should generate flights from cache when same seed is used', () => {
+  it('should return flights from cache when same date is used', () => {
     const flights1 = generateFlightsByDate(isoDate);
     expect(flights1.length).toBeGreaterThan(0);
+    // expect(cacheFlightsMock).toHaveBeenCalledWith(........);
 
     const flights2 = generateFlightsByDate(isoDate);
     expect(flights2.length).toBeGreaterThan(0);
 
     expect(flights1).toEqual(flights2);
+    // expect(cacheFlightsMock).toHaveBeenCalledTimes(1);
   });
 });
