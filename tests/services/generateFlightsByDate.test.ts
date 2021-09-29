196ac62d231e16343013bae2a15f5c79ea052cda
Demondre Livingston <livingstondemondre7@gmail.com>
2021-09-29 16:19:08 -0400

    fixing tests to match suggestions adding getMock

--- tests/services/generateFlightsByDate.test.ts
+++ tests/services/generateFlightsByDate.test.ts
@@ -1,8 +1,33 @@
 import 'jest';
 import { DateTime } from 'luxon';
+import { flightCache } from '../../src/FlightCache';
 import { generateFlightsByDate } from '../../src/services/generateFlightsByDate';
+import { getMock } from '../testUtils/getMock';
+
+const mockFlights = [
+  {
+    flightNumber: '4858',
+    origin: { code: 'DFW', city: 'Dallas-Fort Worth', timezone: 'America/Chicago', location: { latitude: 32.8998, longitude: 97.0403 } },
+    destination: { code: 'PHL', city: 'Philadelphia', timezone: 'America/New_York', location: { latitude: 39.8729, longitude: -75.2437 } },
+    distance: 7393,
+    duration: { locale: '19h 3m', hours: 19, minutes: 3 },
+    departureTime: '2020-01-01T03:46:40.000-05:00',
+    arrivalTime: '2020-01-01T23:49:40.000-04:00',
+    aircraft: { model: '738', passengerCapacity: { total: 160, main: 144, first: 16 }, speed: 400 },
+  },
+];
+jest.mock('../../src/FlightCache.ts', () => ({
+  flightCache: {
+    getFlights: jest.fn().mockReturnValue([]),
+    cacheFlights: jest.fn(),
+  },
+}));
 
 describe('generateFlightsByDate ', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   const dateString = '2021-01-01';
   const isoDate = DateTime.fromISO(dateString, { zone: 'utc' });
 
@@ -11,15 +36,25 @@ describe('generateFlightsByDate ', () => {
     expect(flights.length).toBeGreaterThan(0);
   });
 
-  it('should return flights from cache when same date is used', () => {
+  it('should generate flights the same flights twice when using the same date', () => {
     const flights1 = generateFlightsByDate(isoDate);
     expect(flights1.length).toBeGreaterThan(0);
-    // expect(cacheFlightsMock).toHaveBeenCalledWith(........);
+    const flights2 = generateFlightsByDate(isoDate);
+    expect(flights2.length).toBeGreaterThan(0);
+    expect(flights1).toEqual(flights2);
+  });
+
+  it('should return flights from cache when same date is used', () => {
+    getMock(flightCache.getFlights).mockReturnValueOnce([]);
+    const flights1 = generateFlightsByDate(isoDate);
+    const cacheFlightsMock = getMock(flightCache.cacheFlights);
+
+    expect(flights1.length).toBeGreaterThan(0);
+    getMock(flightCache.getFlights).mockReturnValueOnce(mockFlights);
 
     const flights2 = generateFlightsByDate(isoDate);
     expect(flights2.length).toBeGreaterThan(0);
 
-    expect(flights1).toEqual(flights2);
-    // expect(cacheFlightsMock).toHaveBeenCalledTimes(1);
+    expect(cacheFlightsMock).toHaveBeenCalledTimes(1);
   });
 });
